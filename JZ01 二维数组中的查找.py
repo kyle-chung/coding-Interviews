3f20b5d68cc9ef41b71a2ee786b2d41a3e19db2e
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-10-11 15:12:41 +0800

    Update JZ01 二维数组中的查找.py

--- JZ01 二维数组中的查找.py
+++ JZ01 二维数组中的查找.py
@@ -15,7 +15,7 @@
 
 给定 target = 20，返回 false。
 
-时间复杂度为 O(NM)
+#暴力解法，时间复杂度为 O(NM)
 class Solution:
     # array 二维列表
     def Find(self, target, array):
@@ -28,4 +28,31 @@ class Solution:
                 return True
         if k == -1:
             return False
- 
+
+通过从 “根节点” 开始搜索，遇到比 target 大的元素就向左，反之向右，即可找到目标值 target 。
+
+“根节点” 对应的是矩阵的 “左下角” 和 “右上角” 元素，本文称之为 标志数 ，以 matrix 中的 左下角元素 为标志数 flag ，则有:
+
+若 flag > target ，则 target 一定在 flag 所在 行的上方 ，即 flag 所在行可被消去。
+若 flag < target ，则 target 一定在 flag 所在 列的右方 ，即 flag 所在列可被消去。
+
+算法流程：
+从矩阵 matrix 左下角元素（索引设为 (i, j) ）开始遍历，并与目标值对比：
+当 matrix[i][j] > target 时，执行 i-- ，即消去第 i 行元素；
+当 matrix[i][j] < target 时，执行 j++ ，即消去第 j 列元素；
+当 matrix[i][j] = target 时，返回 truetrue ，代表找到目标值。
+若行索引或列索引越界，则代表矩阵中无目标值，返回 false 。
+每轮 i 或 j 移动后，相当于生成了“消去一行（列）的新矩阵”， 索引(i,j) 指向新矩阵的左下角元素（标志数），因此可重复使用以上性质消去行（列）。
+
+复杂度分析：
+时间复杂度 O(M+N)O(M+N) ：其中，NN 和 MM 分别为矩阵行数和列数，此算法最多循环 M+NM+N 次。
+空间复杂度 O(1)O(1) : i, j 指针使用常数大小额外空间。
+
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        i, j = len(matrix) - 1, 0
+        while i >= 0 and j < len(matrix[0]):
+            if matrix[i][j] > target: i -= 1
+            elif matrix[i][j] < target: j += 1
+            else: return True
+        return False
