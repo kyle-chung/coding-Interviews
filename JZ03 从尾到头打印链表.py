136a61a5eadc8b6d001eeaae60259899ff05dfab
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-10-12 17:55:29 +0800

    Update JZ03 从尾到头打印链表.py

--- JZ03 从尾到头打印链表.py
+++ JZ03 从尾到头打印链表.py
@@ -13,3 +13,11 @@ class Solution:
             stack.append(head.val)
             head = head.next
         return stack[::-1]
+
+方法1：递归法 利用递归将头结点移动到末端，然后回溯，依次将节点的值放入到列表中，既可以实现链表中的值倒序输出。
+O(n)
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        return self.reversePrint(head.next) + [head.val] if head else []
+
