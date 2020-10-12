4b33d3862b65ae31ac7ce8e4b52e508397a932fa
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-10-12 17:47:27 +0800

    Create JZ03 从尾到头打印链表.py

---
+++ JZ03 从尾到头打印链表.py
@@ -0,0 +1,15 @@
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+示例 1：
+
+输入：head = [1,3,2]
+输出：[2,3,1]
+
+# O(n)
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        stack = []
+        while head:
+            stack.append(head.val)
+            head = head.next
+        return stack[::-1]
