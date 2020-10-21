1fea2cf82c6a8e62a1f53c6efc845c47e141b70e
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-10-21 15:09:17 +0800

    Update JZ11	二进制中1的个数.py

--- JZ11	二进制中1的个数.py
+++ JZ11	二进制中1的个数.py
@@ -15,3 +15,39 @@
 输入：11111111111111111111111111111101
 输出：31
 解释：输入的二进制串 11111111111111111111111111111101 中，共有 31 位为 '1'。
+
+# O(n) and O(n)
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        n = bin(n)
+        n = str(n)
+        n = list(n)
+        count = 0
+        for i in range(len(n)):
+            if n[i] == '1':
+                count += 1
+        return count
+
+# 时间复杂度 O(log_2 n),空间复杂度 O(1)O(1)
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            res += n & 1
+            n >>= 1
+         
+&	按位与运算符：参与运算的两个值,如果两个相应位都为1,则该位的结果为1,否则为0
+>>	右移动运算符：把">>"左边的运算数的各二进位全部右移若干位，>> 右边的数字指定了移动的位数
+
+时间复杂度 O(M)：M为二进制数字n中1的个数
+空间复杂度 O(1)
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            res += 1
+            n &= n - 1
+        return res
+
