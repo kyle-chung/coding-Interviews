59e02369f97f06a141c6c93d651af4241f1776bc
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-03 17:01:20 +0800

    Update JZ14 链表中倒数第k个节点.py

--- JZ14 链表中倒数第k个节点.py
+++ JZ14 链表中倒数第k个节点.py
@@ -6,3 +6,23 @@
 给定一个链表: 1->2->3->4->5, 和 k = 2.
 
 返回链表 4->5.
+
+1.遍历链表，将节点存入列表中
+2.返回第-k个节点即可
+class Solution:
+    def getKthFromEnd(self, head: ListNode, k: int) -> ListNode:
+        l = []
+        while head:
+            l.append(head)
+            head = head.next
+        return l[-k]
+      
+设置一个指针走 (n−k) 步
+class Solution:
+    def getKthFromEnd(self, head: ListNode, k: int) -> ListNode:
+        former, latter = head, head
+        for _ in range(k):
+            former = former.next
+        while former:
+            former, latter = former.next, latter.next
+        return latter     
