68f8a0b570b72239d6822457cd85d7a2499e9dd8
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-04 17:51:47 +0800

    Update JZ19 顺时针打印矩阵.py

--- JZ19 顺时针打印矩阵.py
+++ JZ19 顺时针打印矩阵.py
@@ -9,3 +9,35 @@
 输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 输出：[1,2,3,4,8,12,11,10,9,5,6,7]
 
+
+解题思路：
+将矩阵第一行的元素添加到 res 列表里，删除第一行（也就是 matrix 中的第一个列表），然后逆时针旋转（这里通过转置+倒序实现），新的 matrix 的第一行即为接下来要打印的元素。
+(zip is not for interview)
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        while matrix:
+            res += matrix.pop(0)
+            matrix = list(zip(*matrix))[::-1]
+        return res
+
+考虑设定矩阵的“左、上、右、下”四个边界，模拟以上矩阵遍历顺序。
+class Solution:
+    def spiralOrder(self, matrix:[[int]]) -> [int]:
+        if not matrix: return []
+        l, r, t, b, res = 0, len(matrix[0]) - 1, 0, len(matrix) - 1, []
+        while True:
+            for i in range(l, r + 1): res.append(matrix[t][i]) # left to right
+            t += 1
+            if t > b: break
+            for i in range(t, b + 1): res.append(matrix[i][r]) # top to bottom
+            r -= 1
+            if l > r: break
+            for i in range(r, l - 1, -1): res.append(matrix[b][i]) # right to left
+            b -= 1
+            if t > b: break
+            for i in range(b, t - 1, -1): res.append(matrix[i][l]) # bottom to top
+            l += 1
+            if l > r: break
+        return res
+
