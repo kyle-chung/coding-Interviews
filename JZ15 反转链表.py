4a04a7ef26a82b627b151518f2f632083c731776
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-05 15:23:17 +0800

    Update JZ15 反转链表.py

--- JZ15 反转链表.py
+++ JZ15 反转链表.py
@@ -4,3 +4,34 @@
 
 输入: 1->2->3->4->5->NULL
 输出: 5->4->3->2->1->NULL
+
+通过栈，创建一个新链表
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        stack = []
+        if not head:
+            return head
+        # 把head中的值加入栈里
+        while head:
+            stack.append(head.val)
+            head = head.next
+        # 取最后加入的一个，即链表尾作为新的链表头
+        cur = ListNode(stack.pop())
+        # 记住头部索引
+        res = cur
+        # 依次给新链表赋值
+        while stack:
+            res.next = ListNode(stack.pop())
+            res = res.next
+        # 返回新链表头部
+        return cur
+
+双指针
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre, cur = None, head
+        while cur:
+            # "=" 右边的值先全部保存再一次性赋值给 "=" 左边的变量
+            cur.next, pre, cur = pre, cur, cur.next
+        return pre
+
