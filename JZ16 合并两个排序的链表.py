c89a90b9a285a4abb9e81308bb31a074ef677ef0
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-06 18:00:41 +0800

    Update JZ16 合并两个排序的链表.py

--- JZ16 合并两个排序的链表.py
+++ JZ16 合并两个排序的链表.py
@@ -4,3 +4,23 @@
 
 输入：1->2->4, 1->3->4
 输出：1->1->2->3->4->4
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        l = []
+        while l1:
+            l.append(l1.val)
+            l1 = l1.next
+
+        while l2:
+            l.append(l2.val)
+            l2 = l2.next
+
+        l.sort()
+
+        res = ListNode(l[0])
+        temp = res
+        for i in range(len(l) - 1):
+            temp.next = ListNode(l[i+1])
+            temp = temp.next
+        return res  
