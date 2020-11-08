b74bcbac1b6853fa27e7136fa12dc5e4d559caec
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-08 18:03:25 +0800

    Create JZ18 二叉树的镜像.py

---
+++ JZ18 二叉树的镜像.py
@@ -0,0 +1,32 @@
+请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+
+例如输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+镜像输出：
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+示例 1：
+
+输入：root = [4,2,7,1,3,6,9]
+输出：[4,7,2,9,6,3,1]
+
+recur:
+class Solution:
+    def mirrorTree(self, root: TreeNode) -> TreeNode:
+        if not root: return
+        tmp = root.left
+        root.left = self.mirrorTree(root.right)
+        root.right = self.mirrorTree(tmp)
+        return root
+
+
