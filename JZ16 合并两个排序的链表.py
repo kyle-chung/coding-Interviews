3ed3d1ce4a9a0835149af2bb0e0eae9d5656d56d
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-08 13:26:30 +0800

    Update JZ16 合并两个排序的链表.py

--- JZ16 合并两个排序的链表.py
+++ JZ16 合并两个排序的链表.py
@@ -36,15 +36,14 @@ class Solution:
 递归
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if not l1:
+        if not l1: return l2  # 终止条件，直到两个链表都空
+        if not l2: return l1
+        if l1.val <= l2.val:  # 递归调用
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
             return l2
-        if not  l2:
-            return l1
-        if l1.val < l2.val:
-            l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
-        l2.next = self.mergeTwoLists(l1, l2.next)
-        return l2
 
 应用or的执行原理 return x or y等价于return x if x else y
 
