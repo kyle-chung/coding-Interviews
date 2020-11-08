77db8ef67c3fd66619b4e3bccef325aa9d463a08
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-08 16:48:38 +0800

    Update and rename JZ17 树的子结构 to JZ17 树的子结构.py

--- JZ17 树的子结构.py
+++ JZ17 树的子结构.py
@@ -25,3 +25,12 @@ B是A的子结构， 即 A中有出现和B相同的结构和节点值。
 
 输入：A = [3,4,5,1,2], B = [4,1]
 输出：true
+
+class Solution:
+    def isSubStructure(self, A: TreeNode, B: TreeNode) -> bool:
+        def recur(A, B):
+            if not B: return True
+            if not A or A.val != B.val: return False
+            return recur(A.left, B.left) and recur(A.right, B.right)
+
+        return bool(A and B) and (recur(A, B) or self.isSubStructure(A.left, B) or self.isSubStructure(A.right, B))
