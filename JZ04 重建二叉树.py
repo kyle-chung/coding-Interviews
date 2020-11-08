c5c40e8b1b5bdabc6b0a28da09b8d165299f1315
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-08 16:23:26 +0800

    Update JZ04 重建二叉树.py

--- JZ04 重建二叉树.py
+++ JZ04 重建二叉树.py
@@ -21,16 +21,16 @@
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
-        def recur(root, left, right):
-            if left > right: return                               # 递归终止
-            node = TreeNode(preorder[root])                       # 建立根节点
-            i = dic[preorder[root]]                               # 划分根节点、左子树、右子树
-            node.left = recur(root + 1, left, i - 1)              # 开启左子树递归
-            node.right = recur(i - left + root + 1, i + 1, right) # 开启右子树递归
-            return node                                           # 回溯返回根节点
-
-        dic, preorder = {}, preorder
-        for i in range(len(inorder)):
-            dic[inorder[i]] = i
-        return recur(0, 0, len(inorder) - 1)
+        if len(inorder) == 0:
+            return None
+        
+        # 根节点
+        root = TreeNode(preorder[0])
+        # 获取根节点在 inorder 中的索引
+        idx = inorder.index(preorder[0])
+        # 左子树
+        root.left = self.buildTree(preorder[1:idx+1], inorder[:idx])
+        # 右子树
+        root.right = self.buildTree(preorder[idx+1:], inorder[idx+1:])
+        return root
 
