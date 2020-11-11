e97ac5ec635772a6a2e105072db5f49039c581c9
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-11 13:59:50 +0800

    Create JZ26 二叉搜索树与双向链表.py

---
+++ JZ26 二叉搜索树与双向链表.py
@@ -0,0 +1,30 @@
+输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的循环双向链表。要求不能创建任何新的节点，只能调整树中节点指针的指向。
+
+性质：二叉搜索树的中序遍历为 递增序列
+
+# 打印中序遍历
+def dfs(root):
+    if not root: return
+    dfs(root.left)  # 左
+    print(root.val) # 根
+    dfs(root.right) # 右
+
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        def dfs(cur):
+            if not cur: return
+            dfs(cur.left) # 递归左子树
+            if self.pre: # 修改节点引用
+                self.pre.right, cur.left = cur, self.pre
+            else: # 记录头节点
+                self.head = cur
+            self.pre = cur # 保存 cur
+            dfs(cur.right) # 递归右子树
+        
+        if not root: return
+        self.pre = None
+        dfs(root)
+        self.head.left, self.pre.right = self.pre, self.head
+        return self.head
+
+
