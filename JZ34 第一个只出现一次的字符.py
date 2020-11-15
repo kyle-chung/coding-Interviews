1dfd41580319aed754589e9d1c67a48dc8315d01
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-15 12:58:30 +0800

    Create JZ34 第一个只出现一次的字符.py

---
+++ JZ34 第一个只出现一次的字符.py
@@ -0,0 +1,29 @@
+在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+
+示例:
+
+s = "abaccdeff"
+返回 "b"
+
+s = "" 
+返回 " "
+
+class Solution:
+    def firstUniqChar(self, s: str) -> str:
+        dic = {}
+        for c in s:
+            dic[c] = not c in dic
+        for c in s:
+            if dic[c]: return c
+        return ' '
+
+class Solution:
+    def firstUniqChar(self, s: str) -> str:
+        dic = {}
+        for c in s:
+            dic[c] = not c in dic
+            for key,value in d.items()
+        for k, v in dic.items():
+            if v: return k
+        return ' '
+
