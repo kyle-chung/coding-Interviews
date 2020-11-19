13d8ecd72a2ec41c7aef6c533eb4c243cc9c4472
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-19 11:11:12 +0800

    Create JZ36 两个链表的第一个公共节点.py

---
+++ JZ36 两个链表的第一个公共节点.py
@@ -0,0 +1,27 @@
+输入两个链表，找出它们的第一个公共节点。
+
+注意：
+
+如果两个链表没有交点，返回 null.
+在返回结果后，两个链表仍须保持原有的结构。
+可假定整个链表结构中没有循环。
+程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+
+输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+输出：Reference of the node with value = 2
+输入解释：相交节点的值为 2 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+
+集合：先遍历A链表把所有节点都放入集合，然后遍历B，当发现集合长度不增长，说明刚才扔进去得节点是重复节点。
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        s = set()
+        while headA:
+            s.add(headA)
+            headA = headA.next
+        l = len(s)
+        while headB:
+            s.add(headB)
+            if len(s)==l: return headB
+            else: l+=1
+            headB = headB.next
+        return None
