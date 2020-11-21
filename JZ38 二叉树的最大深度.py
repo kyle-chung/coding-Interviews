9a62b34699911842c15063d8b41b92b30e38f42a
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-21 17:32:53 +0800

    Update JZ38 二叉树的最大深度.py

--- JZ38 二叉树的最大深度.py
+++ JZ38 二叉树的最大深度.py
@@ -13,3 +13,47 @@
     /  \
    15   7
 返回它的最大深度 3 。
+
+自创：双stack
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        stack1 = [root]
+        stack2 = []
+        depth = 0
+
+        while stack1 or stack2:
+            if stack1: depth += 1
+            while stack1:
+                temp = stack1.pop()
+                if temp.left:
+                    stack2.append(temp.left)
+                if temp.right:
+                    stack2.append(temp.right)
+
+            if stack2: depth += 1
+            while stack2:
+                temp = stack2.pop()
+                if temp.left:
+                    stack1.append(temp.left)
+                if temp.right:
+                    stack1.append(temp.right)
+
+        return depth
+    
+    recur：
+    class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+        if root == None:
+            return 0
+        
+        left_high = self.maxDepth(root.left)
+        right_high = self.maxDepth(root.right)
+
+        return max(left_high,right_high) + 1
+
+
