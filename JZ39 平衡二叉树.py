64c88dc7d863ea35abc7b57964cb1b2c6f747462
kyle-chung <67317915+kyle-chung@users.noreply.github.com>
2020-11-23 11:11:25 +0800

    Update JZ39 平衡二叉树.py

--- JZ39 平衡二叉树.py
+++ JZ39 平衡二叉树.py
@@ -11,3 +11,18 @@
    15   7
 返回 true 。
 
+先序遍历 + 判断深度 （从顶至底）：与自创一样哦
+
+思路是构造一个获取当前子树的深度的函数 depth(root) （即 JZ38. 二叉树的深度 ）。
+通过比较某子树的左右子树的深度差 abs(depth(root.left) - depth(root.right)) <= 1 是否成立，来判断某子树是否是二叉平衡树。若所有子树都平衡，则此树平衡。
+时间复杂度 O(Nlog N)
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        if not root: return True
+        return abs(self.depth(root.left) - self.depth(root.right)) <= 1 and \
+            self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def depth(self, root):
+        if not root: return 0
+        return max(self.depth(root.left), self.depth(root.right)) + 1
+
