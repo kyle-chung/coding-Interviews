4702ce320d846aaf28246ffa91c9c4946175f762
kyle-cheung <67317915+kyle-chung@users.noreply.github.com>
2021-11-16 23:36:50 +0800

    Update JZ22 从上到下打印二叉树.py

--- JZ22 从上到下打印二叉树.py
+++ JZ22 从上到下打印二叉树.py
@@ -15,15 +15,16 @@
 题目要求的二叉树的 从上至下 打印（即按层打印），又称为二叉树的 广度优先搜索（BFS）
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[int]:
-        stack = [root]
-        res = []
-
-        while stack:
-            temp = stack.pop(0)
-            res.append(temp.val)
-            if temp.left:
-                stack.append(temp.left)
-            if temp.right:
-                    stack.append(temp.right)
-        
+        if not root: return []
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            res.append(node.val)
+            if node.left: queue.append(node.left)
+            if node.right: queue.append(node.right)
         return res
+    
+Python 中使用 collections 中的双端队列 deque() ，其 popleft() 方法可达到 O(1) 时间复杂度
+列表 list 的 pop(0) 方法时间复杂度为 O(N)
+
